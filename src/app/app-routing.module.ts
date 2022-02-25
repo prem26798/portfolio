fa312dc427da0d9c5a74c7dc3cd4724e8032cb98
prem26798 <premkumar26798@gmail.com>
2022-02-25 13:50:56 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContentComponent } from './content/content.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent  } from './education/education.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:ContentComponent},
+  {path: 'education', component: EducationComponent},
+  {path: 'skills', component: SkillsComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '**', component : ContentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroductionComponent,
+    AboutComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    FooterComponent,
+    ContentComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
